fada50b85cbadaf3726fe6fc19aaf3688b0e4c1c
olliekrk <krolik.olgierd@gmail.com>
2020-04-23 23:50:52 +0200

    Trigger build

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been pinged {} f****** times.\n'.format(count)
+    return 'Hello Stranger! I have been pinged {} f****** times.\n'.format(count)
